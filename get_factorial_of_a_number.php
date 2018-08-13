efc6972080964fddf2b0b9acd93a4bf0f2be5172
jitendrayy <jitendray@cybage.com>
2018-08-13 17:34:05 +0530

    Fibonacci series code added

--- get_factorial_of_a_number.php
+++ get_factorial_of_a_number.php
@@ -1,6 +1,10 @@
 <?php
 /**
- * Get factorial of a number
+ * For an integer n greater than or equal to 1, the factorial is the product of all integers less than 
+ * or equal to n but greater than or equal to 1. The factorial value of 0 is defined as equal to 1. The factorial 
+ * values for negative integers are not defined.
+ * 0! = 1, 1! = 1, 2! = 2
+ * Get factorial of a number $n
  * 
  * @param int $n
  * @return int
@@ -9,7 +13,7 @@ function getFactorialOfANumber($n)
 {
     $x = 1;
     for ($i = 1; $i < $n; $i++) {
-        $x *= ($i+1);
+        $x *= ($i + 1);
     }
     return $x;
 }
@@ -24,7 +28,7 @@ function factorial($n)
     if ($n < 2) {
         return 1;
     } else {
-        return $n * factorial($n-1);
+        return $n * factorial($n - 1);
     }
 }
 echo getFactorialOfANumber(5)."\n<br>";
--- get_fibonacci_series.php
+++ get_fibonacci_series.php
@@ -1,5 +1,24 @@
 <?php
+ /**
+  * Fibonacci series:
+  * a series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
+  * The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+  * Draw fibonacci series up to n numbers
+  * 
+  * @param int $n
+  * @return int
+  */
 function getFibonacciSeries($n)
 {
-    
+    $a = 0;
+    $b = 1;
+    $d = $a.' '.$b.' ';
+    for ($i = 2; $i < $n; $i++) {
+        $c = $a + $b;
+        $d .= $c . ' ';
+        $a = $b;
+        $b = $c;
+    }
+    return $d;
 }
+echo getFibonacciSeries(8);
